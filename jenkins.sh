c747791c36e348aa1c226e5b6f4955bafd5c67ee
Harish-exc <gorleharish.exc.com>
2025-11-19 17:46:24 +0530

    For Jenkins installation on Ubuntu

--- jenkins.sh
+++ jenkins.sh
@@ -2,9 +2,6 @@ sudo apt update
 sudo apt install fontconfig openjdk-21-jre
 java -version
 
-openjdk 21.0.8 2025-07-15
-OpenJDK Runtime Environment (build 21.0.8+9-Debian-1)
-OpenJDK 64-Bit Server VM (build 21.0.8+9-Debian-1, mixed mode, sharing)
 
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
