f3c68daa47a2a5ffdb3781eb9aba9747110a03c4
Harish-exc <gorleharish.exc.com>
2025-11-23 14:48:50 +0530

    Terraform installation

---
+++ terrafrom.sh
@@ -0,0 +1,17 @@
+# Update packages
+sudo apt-get update -y
+
+# Install unzip (if not installed)
+sudo apt-get install -y unzip curl
+
+# Download latest Terraform
+curl -LO https://releases.hashicorp.com/terraform/1.7.7/terraform_1.7.7_linux_amd64.zip
+
+# Unzip the downloaded file
+unzip terraform_1.7.7_linux_amd64.zip
+
+# Move terraform binary to /usr/local/bin
+sudo mv terraform /usr/local/bin/
+
+# Verify installation for version
+terraform -v
